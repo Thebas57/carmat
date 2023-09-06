5ce6ebc1de62886c333f697d1bf4e436c293dbc8
Thebas57 <theohelf@gmail.com>
2023-09-06 13:08:06 +0200

    animation image

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,30 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 const Carousel = ({ images, type }) => {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+        duration: 0.5,
+      },
+    },
+  };
+  const Item = {
+    hidden: {
+      scale: 0,
+    },
+    show: {
+      scale: 1,
+      transition: {
+        type: "spring",
+        duration: 2,
+      },
+    },
+  };
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const goToPrevSlide = () => {
@@ -16,23 +40,30 @@ const Carousel = ({ images, type }) => {
   };
 
   return (
-    <div className={type==="produits" ? "carousel" : "carouvisu"}>
+    <motion.div
+      className={type === "produits" ? "carousel" : "carouvisu"}
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
       <button className="prevButton" onClick={goToPrevSlide}>
         &#8249;
       </button>
       {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={`Slide ${index + 1}`}
-          className={index === currentIndex ? "active" : ""}
-          onClick={() => setCurrentIndex(index)}
-        />
+        <motion.div className="div-carou" key={index} variants={Item}>
+          <img
+            key={index}
+            src={image}
+            alt={`Slide ${index + 1}`}
+            className={index === currentIndex ? "active" : ""}
+            onClick={() => setCurrentIndex(index)}
+          />
+        </motion.div>
       ))}
       <button className="nextButton" onClick={goToNextSlide}>
         &#8250;
       </button>
-    </div>
+    </motion.div>
   );
 };
 
