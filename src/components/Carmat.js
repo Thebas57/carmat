531883f169f9d80683e6388b3fee27ffee9e0f20
Thebas57 <theohelf@gmail.com>
2023-09-25 12:39:55 +0200

    animation home

--- src/components/Carmat.js
+++ src/components/Carmat.js
@@ -8,10 +8,10 @@ const Carmat = () => {
   return (
     <motion.div
       className="carmat-container"
-      initial={{ x: "100%" }}
-      animate={{ x: 0 }}
-      exit={{ x: "100%" }}
-      transition={{ duration: 0.6 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
     >
       <div className="carmat">
         <TypeIt
