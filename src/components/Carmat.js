42bbc5f98f75c902c1c81b328f8aaab29e3e0a41
Thebas57 <theohelf@gmail.com>
2023-10-02 12:39:56 +0200

    exemple de bouton

--- src/components/Carmat.js
+++ src/components/Carmat.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import TypeIt from "typeit-react";
-import { GrGallery, GrContactInfo } from "react-icons/gr";
+import { RiContactsFill } from "react-icons/ri";
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 
@@ -38,6 +38,12 @@ const Carmat = () => {
       <div className="direction">
         <NavLink to="/galerie">
           <button className="btn btn-3 btn-3d icon-cog">Galerie</button>
+        </NavLink>
+        <NavLink to="/galerie">
+          <button className="btn-carrelage">GALERIE</button>
+        </NavLink>
+        <NavLink to="/galerie">
+          <button className="btn-carrelage2"><RiContactsFill/></button>
         </NavLink>
         <NavLink to="/contact">
           <button className="btn btn-3 btn-3d icon-cog2">Contact</button>
