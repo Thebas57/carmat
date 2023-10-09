64639e13c36d151f9ba35493c95345f79241d585
Thebas57 <theohelf@gmail.com>
2023-10-09 15:14:23 +0200

    début caroussel

--- src/pages/Galerie.js
+++ src/pages/Galerie.js
@@ -2,21 +2,49 @@ import React, { useState } from "react";
 import Carousel from "../components/Carousel";
 import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
+import Slider from "react-slick";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
+function PrevArrow({ onClick }) {
+  return (
+    <div className="arrow prev" onClick={onClick}>
+      <FaArrowLeft />
+    </div>
+  );
+}
+
+function NextArrow({ onClick }) {
+  return (
+    <div className="arrow next" onClick={onClick}>
+      <FaArrowRight />
+    </div>
+  );
+}
 
 const Galerie = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+
   const images = [
     "./img/galerie/pdt1.jpg",
     "./img/galerie/pdt2.jpg",
     "./img/galerie/pdt3.jpg",
+    "./img/galerie/pdt3.jpg",
+    "./img/galerie/pdt3.jpg",
   ];
 
-  const visuels3D = [
-    "./img/galerie/visuel1.png",
-    "./img/galerie/visuel2.png",
-    "./img/galerie/visuel3.png",
-  ];
-
-  const [galerieSwitch, setSwitch] = useState(false);
+  const settings = {
+    dots: false,
+    infinite: true,
+    lazyload: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
 
   return (
     <motion.div
@@ -26,40 +54,27 @@ const Galerie = () => {
       exit={{ x: "100%" }}
       transition={{ duration: 0.6 }}
     >
-      <div className="header">
-        <span
-          className={
-            galerieSwitch
-              ? "galerie-direction"
-              : "galerie-direction activedirection"
-          }
-          onClick={(e) => setSwitch(false)}
-        >
-          Nos produits
-        </span>
-        <span className="title">Galerie</span>
-        <span
-          className={
-            !galerieSwitch
-              ? "galerie-direction"
-              : "galerie-direction activedirection"
-          }
-          onClick={(e) => setSwitch(true)}
-        >
-          Nos visuels 3D
-        </span>
-      </div>
-      {!galerieSwitch ? (
-        <Carousel images={images} type={"produits"} />
-      ) : (
-        <Carousel images={visuels3D} type={"visuels"} />
-      )}
-      <div className="back-galerie">
+      <video autoPlay muted loop id="background-video">
+        <source src="./img/haloween/fond.mp4" type="video/mp4" />
+      </video>
+      <div className="btn-galerie">
         <NavLink to="/">
           <button className="btn btn-5 btn-5a icon-cart">
             <span>Accueil</span>
           </button>
         </NavLink>
+      </div>
+      <div className="slide-container">
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={index === imageIndex ? "slide activeSlide" : "slide"}
+            >
+              <img src={image} alt={index} />
+            </div>
+          ))}
+        </Slider>
       </div>
     </motion.div>
   );
